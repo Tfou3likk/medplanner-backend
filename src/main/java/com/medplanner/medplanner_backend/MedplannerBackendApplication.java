cf91d7e0c04fb11d1e1d1cfcaaa4c73e40ed2a72
Tfou3likk <lazercity37@gmail.com>
2026-01-03 03:19:22 +0100

    debut du projet

---
+++ src/main/java/com/medplanner/medplanner_backend/MedplannerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.medplanner.medplanner_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedplannerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedplannerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medplanner/medplanner_backend/MedplannerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medplanner.medplanner_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedplannerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
